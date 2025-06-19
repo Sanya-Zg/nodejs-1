1358ab2f88a1aefb3a65bcb8367c2ef94c798bd6
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-19 21:40:59 +0300

    final version

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { connectDB } from './db/connect.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import notFound from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 
 // middleware
@@ -15,8 +16,9 @@ app.use(express.json());
 app.use('/api/v1/tasks', tasks);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const start = async () => {
     try {
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 import Task from '../models/Task.js';
 import asyncWrapper from '../middleware/async.js';
+import { createCustomError } from '../errors/custom-error.js';
 
 export const getAllTasks = asyncWrapper( async (req, res) => {
     const tasks = await Task.find({});
@@ -15,7 +16,7 @@ export const getTask = asyncWrapper(async (req, res) => {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
     if (!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}`});
+        return next(createCustomError(`No task with id: ${taskID}`, 404));
     }
     res.status(200).json({ task }); 
 }) 
@@ -27,7 +28,7 @@ export const updateTask = asyncWrapper(async (req, res) => {
             runValidators: true
         });
         if (!task) {
-            return res.status(404).json({ msg: `No task with id: ${taskID}`});
+            return next(createCustomError(`No task with id: ${taskID}`, 404));
         }
         res.status(200).json({task});
     }) 
@@ -37,7 +38,7 @@ export const deleteTask = asyncWrapper(async (req, res) => {
     const { id: taskID } = req.params;
     const task = await Task.findOneAndDelete({_id: taskID});
     if (!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}`});
+        return next(createCustomError(`No task with id: ${taskID}`, 404));
     }
     res.status(200).json(task);
 }) 
